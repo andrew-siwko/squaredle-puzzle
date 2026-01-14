1d3ee9159f9544050bcbdce65a4894375a07d333
Andrew Siwko <asiwko@siwko.org>
2026-01-14 13:52:47 -0500

    updated code to check content of puzzle and only write if changed

--- sq-extract.py
+++ sq-extract.py
@@ -9,5 +9,20 @@ today_key=list(puzzle_dict.keys())[0]
 today_puzzle=puzzle_dict[today_key]
 today_puzzle_board=[x.upper() for x in today_puzzle['board']]
 puzzle_json={"puzzle_length": 12, "puzzle_text": today_puzzle_board}
-with open('puzzle.json','wt') as f:
-    f.write(json.dumps(puzzle_json))
+with open('puzzle.json','rt') as f:
+    old_puzzle=json.loads(f.read)
+
+puzzle_changed=False
+new_puzzle=puzzle_json['puzzle_text']
+current_puzzle=old_puzzle['puzzle_text']
+if len(new_puzzle)!=len(current_puzzle):
+    print('puzzle length changed')
+    puzzle_changed=True
+
+for i in range(len(current_puzzle)):
+    if current_puzzle[i]!=new_puzzle[i]:
+        print('puzzle texrt changed on row',i+1,current_puzzle[i],'->',new_puzzle[i])
+        puzzle_changed=True
+if puzzle_changed==True:        
+    with open('puzzle.json','wt') as f:
+        f.write(json.dumps(puzzle_json))
